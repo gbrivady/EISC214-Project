a577ce4a1622e3e3145f8a7875b65ef5ad41f39b
Gregory Brivady <gregory.brivady@student.isae-supaero.fr>
2022-04-11 10:47:39 +0200

    update tests for string to token list

--- test/test.c
+++ test/test.c
@@ -5,6 +5,7 @@
 
 void test_str2tok(){
 
+    printf("Begin testing \"token_from_string\" .... \n");
     /*  TESTING SINGLE CHAR TOKENS */
     token* p_token = token_from_string("(");
     print_token(p_token);
@@ -64,10 +65,26 @@ void test_str2tok(){
     print_token(p_token);
     printf("\n");
     free_token(p_token);
+
+    printf("    - Done \n");
+}
+
+void test_token_list(){
+    
+    printf("Begin testing \"read_tokens\" .... \n");
+    char* str = "3*y+5 *(variable - 37) ";
+    token_list* t_list = read_tokens(str);
+    printf("    String successfully read : \n");
+    print_token_list(t_list);
+    putchar('\n');
+    free_token_list(t_list);
+    printf("    - Done \n");
+
 }
 
 int main(int argc, char const *argv[])
 {
     test_str2tok();
+    test_token_list();
     return 0;
 }
