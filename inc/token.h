d2711f3e240d0cd88c51e647d524f0ddefad5dd8
Gregory Brivady <gregory.brivady@student.isae-supaero.fr>
2022-04-11 10:51:30 +0200

    move token lists to token.h

--- inc/token.h
+++ inc/token.h
@@ -38,4 +38,24 @@ void print_token_operation(token* p_token);
 void print_token_variable(token* p_token);
 void print_token_number(token* p_token);
 
+typedef struct token_list_cell
+{
+    token* data;
+    struct token_list_cell* next;
+
+} token_list_cell;
+
+typedef struct token_list
+{
+    token_list_cell* first;
+    token_list_cell* last;
+} token_list;
+
+void append_token(token_list* p_list, token* p_token);
+
+void print_token_list(token_list* p_t_list);
+
+void free_token_list(token_list* p_t_list);
+
+
 #endif
